9f5470556d6c1a3c457f51224d7f9639d92cac99
sweetpark <wooyeong1998@gmail.com>
2026-09-07 18:19:31 +0900

    feat(seo): 구글 서치 콘솔 소유권 확인 메타태그 추가 및 robots.txt 생성 자동화

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -59,6 +59,7 @@ export default (() => {
         )}
         <link rel="preconnect" href="https://cdnjs.cloudflare.com" crossOrigin="anonymous" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-site-verification" content="Ge786HI5EClgz40IdlJ4kshz3Si98PXjN_YjYWGghKQ" />
 
         <meta name="og:site_name" content={cfg.pageTitle}></meta>
         <meta property="og:title" content={title} />
